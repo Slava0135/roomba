10248d58fd8e0bf8194cdb7e490e5904bbf28d5d
slava0135 <super.novalskiy_0135@inbox.ru>
2021-01-21 15:33:11 +0300

    fine

--- scripts/roomba.js
+++ scripts/roomba.js
@@ -10,15 +10,16 @@ roomba.constructor = () => extend(MechUnit, {
 
 roomba.defaultController = () => extend(AIController, {
     updateUnit() {
-        if(random.chance(0.1)){
+        if(random.chance(0.01)){
             let x = random.range(1);
             let y = random.range(1);
             this.vec.set(x, y);
             this.vec.setLength(this.unit.realSpeed());
+            this.vec.set(this.unit.x + this.vec.x, this.unit.y + this.vec.y);
             this.unit.lookAt(this.vec);
         }
-        this.vec.set(Mathf.cosDeg(this.unit.rotation), Mathf.sinDeg(this.unit.rotation));
-        this.vec.setLength(this.unit.realSpeed());
-        this.unit.moveAt(this.vec);
+        //this.vec.set(Mathf.cosDeg(this.unit.rotation), Mathf.sinDeg(this.unit.rotation));
+        //this.vec.setLength(this.unit.realSpeed());
+        //this.unit.moveAt(this.vec);
     },
 });
