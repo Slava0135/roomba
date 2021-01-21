ad5f61cf6bb1ee86cc0f16aea50c79673f37c2a9
slava0135 <super.novalskiy_0135@inbox.ru>
2021-01-21 14:54:39 +0300

    dance

--- scripts/roomba.js
+++ scripts/roomba.js
@@ -12,7 +12,7 @@ roomba.defaultController = () => extend(AIController, {
     updateUnit() {
         let x = random.range(1);
         let y = random.range(1);
-        this.unit.angleTo(x, y);
+        this.unit.lookAt(x, y);
         this.vec.set(x, y);
         this.vec.setLength(this.unit.realSpeed());
         this.unit.moveAt(this.vec)
