bc5e711bb23cfb2a91b2ba5fa0661618d1ca4d4a
slava0135 <super.novalskiy_0135@inbox.ru>
2021-01-21 13:18:59 +0300

    testing

--- scripts/main.js
+++ scripts/main.js
@@ -7,9 +7,12 @@ Events.run(Trigger.update, () => {
             if (team.countType(roomba) < Units.getCap(team.team)) {
                 let x = core.x + random.range(64);
                 let y = core.y + random.range(64);
-                Fx.unitSpawn.at(x, y, 0, roomba);
-                roomba.spawn(team.team, x, y);
-            }
+                let tile = Vars.world.tile(x, y);
+                if (tile == null || tile.building == null){
+                    Fx.unitSpawn.at(x, y, 0, roomba);
+                    roomba.spawn(team.team, x, y);
+                };
+            };
         });
     });
 });
