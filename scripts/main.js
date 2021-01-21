7d48103e703d649e30206c9a374fa1adc672c2d7
slava0135 <super.novalskiy_0135@inbox.ru>
2021-01-21 12:44:04 +0300

    idk

--- scripts/main.js
+++ scripts/main.js
@@ -4,12 +4,12 @@ const roomba = Vars.content.getByName(ContentType.unit, "roomba-roomba");
 const random = Rand(0);
 
 Events.run(Trigger.update, () => {
-    for (let team of Vars.state.teams.present) {
-        for (let core of team.cores) {
+    Vars.state.teams.present.each((team) => {
+        team.cores.each((core) => {
             let x = core.x + random.range(64);
             let y = core.y + random.range(64);
             Fx.unitSpawn.at(x, y, 0, roomba);
             roomba.spawn(team, x, y);
-        }
-    }
+        });
+    });
 });
