0aea170609e2d165b09b704d125c062ee8ed1ba0
slava0135 <super.novalskiy_0135@inbox.ru>
2021-01-21 13:41:27 +0300

    math

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ Events.run(Trigger.update, () => {
             if (team.countType(roomba) < Units.getCap(team.team)) {
                 let x = core.x + random.range(64);
                 let y = core.y + random.range(64);
-                if (!Vars.world.solid(x / 8, y / 8)) {
+                if (!Vars.world.solid(Mathf.round(x / 8), Mathf.round(y / 8))) {
                     Fx.unitSpawn.at(x, y, 0, roomba);
                     roomba.spawn(team.team, x, y);
                 };
