794c734c7f9aff37871c6fd2cf85df670976d59c
slava0135 <super.novalskiy_0135@inbox.ru>
2021-01-23 17:41:55 +0300

    limited roomba amount to 100

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@ Events.run(Trigger.update, () => {
     let roomba = Vars.content.getByName(ContentType.unit, "roomba-roomba");
     Vars.state.teams.present.each((team) => {
         team.cores.each((core) => {
-            if (team.countType(roomba) < Units.getCap(team.team)) {
+            if (team.countType(roomba) < Units.getCap(team.team) && team.countType(roomba) < 100) {
                 let x = core.x + random.range(64);
                 let y = core.y + random.range(64);
                 if (!Vars.world.solid(Mathf.round(x / 8), Mathf.round(y / 8))) {
